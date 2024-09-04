f170bf2e14244dedfccff6144f2905fbc2d4c38b
Mvq <mvpberger@gmail.com>
2024-09-04 14:30:40 +0200

    h

--- ex2.py
+++ ex2.py
@@ -8,13 +8,13 @@ arlo = robot.Robot()
 print("Running ...")
 
 speed = 60
-k=2
+k=5
 time = 0
 for i in range(0, 2*k):
   if i == 0:
     time = 6.45
   else:
-    time = 6.2
+    time = 6.25
   
   if not (i % 2):
     arlo.go_diff(speed*2-3, speed, 1, 1)
