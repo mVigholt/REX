15383a63ac1f220cb9cab72d72bab80d35b95871
Mvq <mvpberger@gmail.com>
2024-09-04 13:57:14 +0200

    ex2

---
+++ ex2.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+import robot
+import numpy as np
+# Create a robot object and initialize
+arlo = robot.Robot()
+
+print("Running ...")
+
+speed = 64
+
+arlo.go_diff(speed, speed/2, 1, 1)
+sleep(3)
+arlo.stop()
