ef6aa82907f03d037f4bf9dd366dfce5f189e507
Mvq <mvpberger@gmail.com>
2024-09-09 16:02:13 +0200

    h

--- ex2.py
+++ ex2.py
@@ -24,7 +24,7 @@ def DriveStraight():
 # left: dir = 0
 # right: dir = 1
 def Rotate(dir):
-  if (int(dir) != 0 & int(dir) != 1):
+  if (int(dir) != 0 or int(dir) != 1):
     print(dir)
     print("dir has to be 1 or 0")
     arlo.stop()
