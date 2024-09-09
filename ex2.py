025fc97804909ff07f0efcccd923705f7dc85e33
mVigholt <zwc822@alumni.ku.dk>
2024-09-09 16:53:12 +0200

    good stuff

--- ex2.py
+++ ex2.py
@@ -9,6 +9,7 @@ arlo = robot.Robot()
 
 print("Running ...")
 
+rotateSpeed = 45
 speed = 60
 error = 2
 
@@ -31,33 +32,30 @@ def Rotate(dir):
     return
   
   if (dir == 1):
-    arlo.go_diff(int(speed-error)/2, int(speed/2), 1, 0)
+    arlo.go_diff(rotateSpeed-error, rotateSpeed, 1, 0)
   else:
-    arlo.go_diff(int(speed-error)/2, int(speed/2), 0, 1)
+    arlo.go_diff(rotateSpeed-error, rotateSpeed, 0, 1)
 
 def run():
-  # safetyStraightDistance = 700
-  # safetySideDistance = 500
-  # starttime = t.time()
-  # isRotating = False
+  safetyStraightDistance = 700
+  safetySideDistance = 500
+  starttime = t.time()
+  isRotating = False
   
-  # while (t.time() - starttime < 20):
-  #   data = GetSensorData()
-  #   if (data[1] > safetyStraightDistance and data[0] > safetySideDistance and data[2] > safetySideDistance):
-  #     isRotating = False
-  #     DriveStraight()
-  #   else:
-  #     if not isRotating:
-  #       isRotating = True
-  #       if (data[0] > data[2]):
-  #         Rotate(0)
-  #       else:
-  #         Rotate(1)
+  while (t.time() - starttime < 20):
+    data = GetSensorData()
+    if (data[1] > safetyStraightDistance and data[0] > safetySideDistance and data[2] > safetySideDistance):
+      isRotating = False
+      DriveStraight()
+    else:
+      if not isRotating:
+        isRotating = True
+        if (data[0] > data[2]):
+          Rotate(0)
+        else:
+          Rotate(1)
         
-  #       if data[1] <= safetyStraightDistance:
-  #         sleep()
-  #   sleep(0.1)
-  arlo.go_diff(45-error, 45, 1, 0)
-  sleep(0.7)
-  arlo.stop()
+        if data[1] <= safetyStraightDistance:
+          sleep(0.65)
+    sleep(0.1)
 run()
