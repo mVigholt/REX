d1e8d308c07690c3010a8ecd9eb0d1c8a3620cce
Mvq <mvpberger@gmail.com>
2024-09-18 16:15:58 +0200

    h

--- ex3.py
+++ ex3.py
@@ -4,8 +4,15 @@
 import cv2 # Import the OpenCV library
 import time as t
 import robot
-import gc
+from pprint import *
 # import psutil
+
+try:
+    import picamera2
+    print("Camera.py: Using picamera2 module")
+except ImportError:
+    print("Camera.py: picamera2 module not available")
+    exit(-1)
 
 # Create a robot object and initialize
 arlo = robot.Robot()
@@ -16,22 +23,39 @@ error = 2
 safetyStraightDistance = 500
 safetySideDistance = 400
 
+# Open a camera device for capturing
+imageSize = (1280, 720)
+FPS = 30
+cam = picamera2.Picamera2()
+frame_duration_limit = int(1/FPS * 1000000) # Microseconds
+# Change configuration to set resolution, framerate
+picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
+                                                            controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+                                                            queue=False)
+cam.configure(picam2_config) # Not really necessary
+cam.start(show_preview=False)
+
+pprint(cam.camera_configuration()) # Print the camera configuration in use
+
+t.sleep(1)  # wait for camera to setup
 
 
-def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=10):
-    """Utility function for setting parameters for the gstreamer camera pipeline"""
-    return (
-        "libcamerasrc !"
-        "videobox autocrop=true !"
-        "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "videoconvert ! "
-        "appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-        )
-    )
+
+
+# def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=10):
+#     """Utility function for setting parameters for the gstreamer camera pipeline"""
+#     return (
+#         "libcamerasrc !"
+#         "videobox autocrop=true !"
+#         "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+#         "videoconvert ! "
+#         "appsink"
+#         % (
+#             capture_width,
+#             capture_height,
+#             framerate,
+#         )
+#     )
     
 def calc_distance(real_size, pixel_size):
     f = 145
@@ -56,7 +80,7 @@ def Rotate(dir):
 
 print("OpenCV version = " + cv2.__version__)
 # Open a camera device for capturing
-cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
+# cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
 
 
 if not cam.isOpened(): # Error
@@ -77,11 +101,12 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     starttime = t.time()
     arlo.stop()
     
-    retval, frameReference = cam.read() # Read frame
+    # retval, frameReference = cam.read() # Read frame
+    image = cam.capture_array("main")
     
-    if not retval: # Error
-        print(" < < <  Game over!  > > > ")
-        exit(-1)
+    # if not retval: # Error
+    #     print(" < < <  Game over!  > > > ")
+    #     exit(-1)
     
     corners, ids, _ = cv2.aruco.detectMarkers(frameReference, aruco_dict)
     
@@ -91,7 +116,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     # memory_info = psutil.virtual_memory()
     
     # print(f"{memory_info.percent}")
-    print(len(corners))    
+        
     if (len(corners) > 0):
         print("stop")
         arlo.stop()
@@ -101,8 +126,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     t.sleep(0.2)
     # [0][0][0] = top left corner
     # [0][1][0] = bottom left corner
-    del frameReference
-    gc.collect()
+    
     # if corners:
         # distance = calc_distance(145, corners[0][0][1][0] - corners[0][0][0][0])
         
