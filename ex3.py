69f099c102df081a8e009c854d00087a7bd35ba9
Mvq <mvpberger@gmail.com>
2024-09-18 16:24:01 +0200

    h

--- ex3.py
+++ ex3.py
@@ -4,7 +4,15 @@
 import cv2 # Import the OpenCV library
 import time as t
 import robot
+from pprint import *
 # import psutil
+
+try:
+    import picamera2
+    print("Camera.py: Using picamera2 module")
+except ImportError:
+    print("Camera.py: picamera2 module not available")
+    exit(-1)
 
 # Create a robot object and initialize
 arlo = robot.Robot()
@@ -15,22 +23,39 @@ error = 2
 safetyStraightDistance = 500
 safetySideDistance = 400
 
+# Open a camera device for capturing
+imageSize = (1280, 720)
+FPS = 30
+cam = picamera2.Picamera2()
+frame_duration_limit = int(1/FPS * 1000000) # Microseconds
+# Change configuration to set resolution, framerate
+picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
+                                                            controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+                                                            queue=False)
+cam.configure(picam2_config) # Not really necessary
+cam.start(show_preview=False)
+
+pprint(cam.camera_configuration()) # Print the camera configuration in use
+
+t.sleep(1)  # wait for camera to setup
 
 
-def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
-    """Utility function for setting parameters for the gstreamer camera pipeline"""
-    return (
-        "libcamerasrc !"
-        "videobox autocrop=true !"
-        "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "videoconvert ! "
-        "appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-        )
-    )
+
+
+# def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=10):
+#     """Utility function for setting parameters for the gstreamer camera pipeline"""
+#     return (
+#         "libcamerasrc !"
+#         "videobox autocrop=true !"
+#         "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+#         "videoconvert ! "
+#         "appsink"
+#         % (
+#             capture_width,
+#             capture_height,
+#             framerate,
+#         )
+#     )
     
 def calc_distance(real_size, pixel_size):
     f = 145
@@ -55,12 +80,12 @@ def Rotate(dir):
 
 print("OpenCV version = " + cv2.__version__)
 # Open a camera device for capturing
-cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
+# cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
 
 
-if not cam.isOpened(): # Error
-    print("Could not open camera")
-    exit(-1)
+# if not cam.isOpened(): # Error
+#     print("Could not open camera")
+#     exit(-1)
 
 # Open a window
 WIN_RF = "Example 1"
@@ -74,25 +99,31 @@ Rotate(1)
 
 while cv2.waitKey(4) == -1: # Wait for a key pressed event
     starttime = t.time()
-    retval, frameReference = cam.read() # Read frame
+    arlo.stop()
     
-    if not retval: # Error
-        print(" < < <  Game over!  > > > ")
-        exit(-1)
+    # retval, frameReference = cam.read() # Read frame
+    frameReference = cam.capture_array("main")
+    
+    # if not retval: # Error
+    #     print(" < < <  Game over!  > > > ")
+    #     exit(-1)
     
     corners, ids, _ = cv2.aruco.detectMarkers(frameReference, aruco_dict)
     
     # Draw markers on the frame if found
     frameReference = cv2.aruco.drawDetectedMarkers(frameReference, corners, ids)
-    print(starttime - t.time(), "\n")
+    print(t.time() - starttime, "\n")
     # memory_info = psutil.virtual_memory()
     
     # print(f"{memory_info.percent}")
         
     if (len(corners) > 0):
+        print("stop")
         arlo.stop()
         break
     
+    Rotate(1)
+    t.sleep(0.2)
     # [0][0][0] = top left corner
     # [0][1][0] = bottom left corner
     
@@ -108,6 +139,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     cv2.imshow(WIN_RF, frameReference)
 
     # t.sleep(0.25)
+    
 
 cv2.imwrite("OttosView.png", frameReference)
 # Finished successfully
