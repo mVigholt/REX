e114453c772b7f95307022d52ac18cb03fcad887
Mvq <mvpberger@gmail.com>
2024-09-18 16:33:37 +0200

    h

--- ex3.py
+++ ex3.py
@@ -4,7 +4,6 @@
 import cv2 # Import the OpenCV library
 import time as t
 import robot
-from pprint import *
 # import psutil
 
 try:
@@ -23,40 +22,53 @@ error = 2
 safetyStraightDistance = 500
 safetySideDistance = 400
 
-# Open a camera device for capturing
-imageSize = (1024, 720)
-FPS = 30
-cam = picamera2.Picamera2()
-frame_duration_limit = int(1/FPS * 1000000) # Microseconds
-# Change configuration to set resolution, framerate
-picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
-                                                            controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
-                                                            queue=False)
-cam.configure(picam2_config) # Not really necessary
-cam.start(show_preview=False)
 
-pprint(cam.camera_configuration()) # Print the camera configuration in use
+##---------------------------------------------------------------------------------------------------------------------------------------
 
-t.sleep(1)  # wait for camera to setup
+# # Open a camera device for capturing
+# imageSize = (1024, 720)
+# FPS = 30
+# cam = picamera2.Picamera2()
+# frame_duration_limit = int(1/FPS * 1000000) # Microseconds
+# # Change configuration to set resolution, framerate
+# picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
+#                                                             controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+#                                                             queue=False)
+# cam.configure(picam2_config) # Not really necessary
+# cam.start(show_preview=False)
+
+# print(cam.camera_configuration()) # Print the camera configuration in use
+
+# t.sleep(1)  # wait for camera to setup
+
+##-----------------------------------------------------------------------------------------
+
+def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=10):
+    """Utility function for setting parameters for the gstreamer camera pipeline"""
+    return (
+        "libcamerasrc !"
+        "videobox autocrop=true !"
+        "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "videoconvert ! "
+        "appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+        )
+    )
+
+Open a camera device for capturing
+cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
+
+if not cam.isOpened(): # Error
+    print("Could not open camera")
+    exit(-1)
+ 
+ 
+##-----------------------------------------------------------------------------------------   
 
 
-
-
-# def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=10):
-#     """Utility function for setting parameters for the gstreamer camera pipeline"""
-#     return (
-#         "libcamerasrc !"
-#         "videobox autocrop=true !"
-#         "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-#         "videoconvert ! "
-#         "appsink"
-#         % (
-#             capture_width,
-#             capture_height,
-#             framerate,
-#         )
-#     )
-    
 def calc_distance(real_size, pixel_size):
     f = 145
     return (f*real_size)/pixel_size
@@ -79,13 +91,7 @@ def Rotate(dir):
     arlo.go_diff(rotateSpeed, rotateSpeed, 0, 1)
 
 print("OpenCV version = " + cv2.__version__)
-# Open a camera device for capturing
-# cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
 
-
-# if not cam.isOpened(): # Error
-#     print("Could not open camera")
-#     exit(-1)
 
 # Open a window
 WIN_RF = "Example 1"
@@ -101,12 +107,13 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     starttime = t.time()
     arlo.stop()
     
-    # retval, frameReference = cam.read() # Read frame
-    frameReference = cam.capture_array("main")
+    # frameReference = cam.capture_array("main")
     
-    # if not retval: # Error
-    #     print(" < < <  Game over!  > > > ")
-    #     exit(-1)
+    retval, frameReference = cam.read() # Read frame
+    
+    if not retval: # Error
+        print(" < < <  Game over!  > > > ")
+        exit(-1)
     
     corners, ids, _ = cv2.aruco.detectMarkers(frameReference, aruco_dict)
     
