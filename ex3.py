6bb10a4237e7e663b7dc33709c4ee5445d9fc719
Mvq <mvpberger@gmail.com>
2024-09-23 09:02:50 +0200

    h

--- ex3.py
+++ ex3.py
@@ -99,7 +99,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     corners, ids, _ = cv2.aruco.detectMarkers(frameReference, aruco_dict)
     
     if (len(corners) > 0):
-        print(cv2.aruco.estimatePoseSingleMarkers(corners, f, frameReference, camera_matrix, distCoeffs))
+        print(cv2.aruco.estimatePoseSingleMarkers(corners, f, camera_matrix, distCoeffs))
         print("stop")
         arlo.stop()
         break
