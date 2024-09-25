47ceb37db1a65d078b8b929ac8af086f4978d86c
Mvq <mvpberger@gmail.com>
2024-09-25 13:37:12 +0200

    h

--- help.py
+++ help.py
@@ -139,3 +139,10 @@ def Turn(dir):
 
 def Stop():
     arlo.stop()
+
+#----------------------------------------------------------------
+class robot:
+  def __init__(self, x = 0, y = 0, theta = 0):
+    self.x = x
+    self.y = y
+    self.theta = theta
