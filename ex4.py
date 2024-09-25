f1e5df835c70b00f575894073ae5707ad36c9973
Mvq <mvpberger@gmail.com>
2024-09-25 13:54:20 +0200

    h

--- ex4.py
+++ ex4.py
@@ -13,27 +13,6 @@ f = 1138
 ottoRadius = 25
 boxRadius = 35
 
-# load dictionary and parameters
-aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-
-# initialize camera transformation matrix
-camera_matrix = np.array([[f, 0, help.cw/2],
-                         [0, f, help.ch/2],
-                         [0, 0, 1]])
-
-
-def getCoordinates(frameReference, aruco_dict, camera_matrix, X):
-  # Assuming no lens distortion for now
-  distCoeffs = np.zeros((5, 1))
-  coordinates = []
-  corners, ids, _ = cv2.aruco.detectMarkers(frameReference, aruco_dict)
-  _, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, X, camera_matrix, distCoeffs)
-
-  ids_flattened = list(itertools.chain(*ids))
-  tvecs_flattened = list(itertools.chain(*tvecs))
-  
-  return ids_flattened, [tvecs_flattened[0], tvecs_flattened[2]]
-
 while cv2.waitKey(4) == -1: # Wait for a key pressed event
   
   retval, frameReference = cam.read() # Read frame
@@ -42,7 +21,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
       print(" < < <  Game over!  > > > ")
       exit(-1)
   
-  ids, tvecs = getCoordinates(frameReference, aruco_dict, camera_matrix, X)
+  ids, tvecs = cam.next_map()
   
-  print(tvecs)
+  print([tvecs[0], tvecs[2]])
   
