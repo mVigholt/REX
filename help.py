0d37444eee4deb3e61c97c266adc6b2ebff12bd3
mVigholt <zwc822@alumni.ku.dk>
2024-09-30 15:20:27 +0200

    q

--- help.py
+++ help.py
@@ -89,7 +89,7 @@ class Cam (object):
             test = []
             print(flat_rvecs)
             for rvec in flat_rvecs: 
-                test.append([math.cos(box_v+rvec[1])*box_c, math.sin(box_v+rvec[1])*box_c])
+                test.append([math.cos(rvec[1])*box_x, math.sin(rvec[1])*box_z])
             print(test)
         return self.flatten(self.ids), flat_tvecs
             
