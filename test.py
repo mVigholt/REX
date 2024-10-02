db74e4c8b2130d7aa67ed415c80d6ecf06e0a44b
mVigholt <zwc822@alumni.ku.dk>
2024-10-02 14:03:35 +0200

    q

--- test.py
+++ test.py
@@ -20,7 +20,6 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     
     #_, corners, ids = cam.next_frame_with_detection(ret_corner=True, ret_id=True)
     ids, tvecs = cam.next_map()
-    print(tvecs)
     t.sleep(1)
 
     # if ids is not None:
