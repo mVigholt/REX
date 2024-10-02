3d492428a8ac8a196836684af7bbe1160ca42e17
Mvq <mvpberger@gmail.com>
2024-10-02 11:34:01 +0200

    h

--- accelerationTest.py
+++ accelerationTest.py
@@ -2,9 +2,16 @@ from time import sleep
 
 import robot
 import numpy as np
+import cv2
 # Create a robot object and initialize
 arlo = robot.Robot()
 
-arlo.go_diff(30, 31, 1, -1)
-sleep(0.1)
-arlo.stop()
+i = 0
+
+while 1 == 1:
+  
+  i += 0.1
+  input(f"you are testing i = {i}. Waiting for input")
+  arlo.go_diff(30, 31, 1, -1)
+  sleep(i)
+  arlo.stop()
