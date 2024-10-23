c17c7b1a0a11e4684510cce395014e2669ed08af
Mvq <mvpberger@gmail.com>
2024-10-23 10:27:12 +0200

    h

--- help.py
+++ help.py
@@ -104,7 +104,7 @@ class Cam (object):
         if self.WIN_RF is None:
             self.__setup_stream()
         # Draw markers on the frame if found
-        frameDrawing = cv2.aruco.drawDetectedMarkers(self.frameReference, self.corners, self.ids)
+        # frameDrawing = cv2.aruco.drawDetectedMarkers(self.frameReference, self.corners, self.ids)
         rvecs, tvecs, _  = cv2.aruco.estimatePoseSingleMarkers(self.corners, X, cam_matrix, distCoeffs)
         for i in range(len(self.ids)):
             frameDrawing = cv2.drawFrameAxes(self.frameReference, cam_matrix, distCoeffs, rvecs[i], tvecs[i], 0.05)
