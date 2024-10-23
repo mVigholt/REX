d8cae4331bf45f40657c8dbc50458d9851ffb320
Mvq <mvpberger@gmail.com>
2024-10-23 15:32:45 +0200

    h

--- help.py
+++ help.py
@@ -111,14 +111,14 @@ class Cam (camera.Camera):
         if flat_tvecs and flat_rvecs is not None:
             flat_tvecs = np.delete(np.array(flat_tvecs), 1, 1)
             for rvec, tvec in zip(flat_rvecs, flat_tvecs): 
-                print(tvec)
-                print(math.dist([0,0], tvec))
+                print("local tvec:", tvec)
+                print("distance: ", math.dist([0,0], tvec))
                 rotation_matrix, _ = cv2.Rodrigues(rvec)
                 euler_angles = rotation_matrix_to_euler_angles(rotation_matrix)
                 tvec = ToGlobal(tvec, euler_angles[1], np.array([-145/2, 115]))
                 print("angle: ", euler_angles)
-                print(tvec)
-                print(math.dist([0,0], tvec))
+                print("global tvec: ", tvec)
+                print("global distance: ", math.dist([0,0], tvec))
             flat_tvecs[:, 1] = flat_tvecs[:, 1] + robotRadius
         else:
             print("flat_tvecs or flat_rvecs was None")
