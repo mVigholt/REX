50d1ebf5d38553e6b3d086541e48c55f147813e1
Mvq <mvpberger@gmail.com>
2024-10-23 10:13:52 +0200

    h

--- test.py
+++ test.py
@@ -20,10 +20,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     
     #_, corners, ids = cam.next_frame_with_detection(ret_corner=True, ret_id=True)
     # ids, tvecs = cam.next_map()
-    cam.__setup_stream()
     cam.stream()
-    
-    print(tvecs)
     t.sleep(1)
 
     # if ids is not None:
