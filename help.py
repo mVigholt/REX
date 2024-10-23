daa117835710ec2e021b9201b11bb41be4981d13
Mvq <mvpberger@gmail.com>
2024-10-23 13:38:02 +0200

    h

--- help.py
+++ help.py
@@ -111,7 +111,7 @@ class Cam (camera.Camera):
                 euler_angles = rotation_matrix_to_euler_angles(rotation_matrix)
                 tvec = ToGlobal(tvec, euler_angles[1], np.array([145/2, 115]))
                 print("angle: ", euler_angles)
-            print(math.dist([0,0,0], flat_tvecs[0]))
+            print(math.dist([0,0], flat_tvecs[0]))
             flat_tvecs[:, 1] = flat_tvecs[:, 1] + robotRadius
         else:
             print("flat_tvecs or flat_rvecs was None")
