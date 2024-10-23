92d96cfe8a6b0a0f3ba30b8a2e673a33b3157a21
mVigholt <zwc822@alumni.ku.dk>
2024-10-23 13:06:31 +0200

    1

--- help.py
+++ help.py
@@ -115,13 +115,13 @@ class Cam (camera.Camera):
             self.__setup_stream()
         print(self.landmarkIds)
         # Draw markers on the frame if found
-        cv2.aruco.drawDetectedMarkers(self.frameReference, self.landmarkCorners, self.landmarkIds)
-        rvecs, tvecs, _  = cv2.aruco.estimatePoseSingleMarkers(self.landmarkCorners, X, cam_matrix, distCoeffs)
-        for i in range(len(self.landmarkIds)):
-            cv2.drawFrameAxes(self.frameReference, cam_matrix, distCoeffs, rvecs[i], tvecs[i], 100, 2)
-        # Stream frames
+        hej = cv2.aruco.drawDetectedMarkers(self.frameReference, self.landmarkCorners, self.landmarkIds)
+        # rvecs, tvecs, _  = cv2.aruco.estimatePoseSingleMarkers(self.landmarkCorners, X, cam_matrix, distCoeffs)
+        # for i in range(len(self.landmarkIds)):
+        #     cv2.drawFrameAxes(self.frameReference, cam_matrix, distCoeffs, rvecs[i], tvecs[i], 100, 2)
+        # # Stream frames
         
-        cv2.imshow(self.WIN_RF, self.frameReference)
+        cv2.imshow(self.WIN_RF, hej)
         
   
 class Timed_lap (object):
