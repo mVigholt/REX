f7daf9740aaf9fa15889cfd3ee4105f1e5aaefcc
Mvq <mvpberger@gmail.com>
2024-10-24 17:06:49 +0200

    h

--- EXAM/camera.py
+++ EXAM/camera.py
@@ -394,8 +394,10 @@ class Camera(object):
             flat_tvecs[:, 1] = flat_tvecs[:, 1] + 450/2
         
         for tvec, flat_tvec in zip(self.tvecs, flat_tvecs):
+            print("before: ", tvec)
             tvec[0][0] = flat_tvec[0]
             tvec[0][2] = flat_tvec[1]
+            print("after: ", tvec)
         ##############################################################################################
         # HOMEBREW, DELETE BY SITE
         ##############################################################################################
