e0cb10679b0dd993e8b61d2b33f2254489fbfddf
Mvq <mvpberger@gmail.com>
2024-10-24 11:48:58 +0200

    h

--- ex5/selflocalize.py
+++ ex5/selflocalize.py
@@ -209,7 +209,7 @@ try:
             rob = robot_models.PointMassModel(ctrl_range=[-path_res, path_res])
             
             _, local_coords = cam.next_map() # her indsætter vi det globale koordinat system konverteret til lokalt
-            local_goal = h.ToLocal(np.array[(est_pose.getX(), est_pose.getY())], est_pose.getTheta(), np.array([75, 0])) # her konverterer vi (75, 0) til et eller andet lokalt koordinat
+            local_goal = h.ToLocal(np.array([est_pose.getX(), est_pose.getY()]), est_pose.getTheta(), np.array([75, 0])) # her konverterer vi (75, 0) til et eller andet lokalt koordinat
             print("local goal: ", local_goal)
             map = m.landmark_map(low=(-2000, 0), high=(2000, 2000), landMarks=local_coords)
             rrt = rt.RRT(start=[0, 0],
