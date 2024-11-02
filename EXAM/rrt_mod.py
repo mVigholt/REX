d7364ace98f8fa830c878a85eeb0f2d53352f079
mVigholt <zwc822@alumni.ku.dk>
2024-11-02 15:54:36 +0100

    1

--- EXAM/rrt_mod.py
+++ EXAM/rrt_mod.py
@@ -66,7 +66,16 @@ class RRT:
 
         self.node_list = [self.start]
 
-        for i in range(self.max_iter):                      
+        for i in range(self.max_iter):  
+            # try to steer towards goal NO MATTER THE EXPAND DIS
+            #--------------------------------------------------------------------
+            final_node = self.steer(self.node_list[-1], 
+                                    self.end,
+                                    np.linalg.norm(np.array(self.node_list[-1].pos) - np.array(self.end.pos)) + self.expand_dis)
+            if self.check_collision_free(final_node):
+                return self.generate_final_course(len(self.node_list) - 1)
+            #--------------------------------------------------------------------
+                                
             rnd_node = self.get_random_node()
             nearest_ind = self.get_nearest_node_index(self.node_list, rnd_node)
             nearest_node = self.node_list[nearest_ind]
@@ -76,22 +85,13 @@ class RRT:
             if self.check_collision_free(new_node):
                 self.node_list.append(new_node)
                 
-            # # try to steer towards goal NO MATTER THE EXPAND DIS
-
-            #try to steer towards the goal if we are already close enough
-            if self.node_list[-1].calc_distance_to(self.end) <= self.expand_dis:
-                final_node = self.steer(self.node_list[-1], self.end,
-                                        self.expand_dis) 
-                if self.check_collision_free(final_node):
-                    return self.generate_final_course(len(self.node_list) - 1)
-                
             # try to steer towards goal NO MATTER THE EXPAND DIS
-            # #--------------------------------------------------------------------
-            # final_node = self.steer(self.node_list[-1], self.end,
-            #                         np.linalg.norm(np.array(self.node_list[-1].pos) - np.array(self.end.pos)))
-            # if self.check_collision_free(final_node):
-            #     return self.generate_final_course(len(self.node_list) - 1)
-            # #--------------------------------------------------------------------
+            # #try to steer towards the goal if we are already close enough
+            # if self.node_list[-1].calc_distance_to(self.end) <= self.expand_dis:
+            #     final_node = self.steer(self.node_list[-1], self.end,
+            #                             self.expand_dis) 
+            #     if self.check_collision_free(final_node):
+            #         return self.generate_final_course(len(self.node_list) - 1)
             
 
             if animation:
