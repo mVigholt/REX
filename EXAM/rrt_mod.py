ce74ce02379aca9a2910940969e54c342d9cecc3
mVigholt <zwc822@alumni.ku.dk>
2024-11-02 15:24:27 +0100

    1

--- EXAM/rrt_mod.py
+++ EXAM/rrt_mod.py
@@ -78,19 +78,20 @@ class RRT:
                 
             # # try to steer towards goal NO MATTER THE EXPAND DIS
 
-            # #try to steer towards the goal if we are already close enough
-            # if self.node_list[-1].calc_distance_to(self.end) <= self.expand_dis:
-            #     final_node = self.steer(self.node_list[-1], self.end,
-            #                             self.expand_dis) 
-            #     if self.check_collision_free(final_node):
-            #         return self.generate_final_course(len(self.node_list) - 1)
+            #try to steer towards the goal if we are already close enough
+            if self.node_list[-1].calc_distance_to(self.end) <= self.expand_dis:
+                final_node = self.steer(self.node_list[-1], self.end,
+                                        self.expand_dis) 
+                if self.check_collision_free(final_node):
+                    return self.generate_final_course(len(self.node_list) - 1)
+                
             # try to steer towards goal NO MATTER THE EXPAND DIS
-            #--------------------------------------------------------------------
-            final_node = self.steer(self.node_list[-1], self.end,
-                                    np.linalg.norm(np.array(self.node_list[-1].pos) - np.array(self.end.pos)))
-            if self.check_collision_free(final_node):
-                return self.generate_final_course(len(self.node_list) - 1)
-            #--------------------------------------------------------------------
+            # #--------------------------------------------------------------------
+            # final_node = self.steer(self.node_list[-1], self.end,
+            #                         np.linalg.norm(np.array(self.node_list[-1].pos) - np.array(self.end.pos)))
+            # if self.check_collision_free(final_node):
+            #     return self.generate_final_course(len(self.node_list) - 1)
+            # #--------------------------------------------------------------------
             
 
             if animation:
