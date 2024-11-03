b1d0055892c14c9a26c04a2e5f426b53a0206f71
Mvq <mvpberger@gmail.com>
2024-11-03 19:26:13 +0100

    h

--- help.py
+++ help.py
@@ -179,11 +179,12 @@ class Arlo (object):
         self.arlo = robot.Robot()
         
     def degreeFunction(self, radian):
-        return max(0, (0.0239*(radian * 180 / math.pi) + 0.0287))
+        return max(0, (radian * 180 / math.pi - 0.23)/44.15)
+        # return max(0, (0.0239*(radian * 180 / math.pi) + 0.0287))
     
     def distanceFunction(self, distance):
-        # return max(0, (distance + 1.7)/355.3)
-        return max(0, (0.00303*distance - 0.1119))
+        return max(0, (distance + 1.7)/355.3)
+        # return max(0, (0.00303*distance - 0.1119))
     
     def Forward(self, distance): 
         self.arlo.go_diff(speed-error, speed, 1, 1)
