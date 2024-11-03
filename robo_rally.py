05a86e3703c0659425eae44844d8944ae8304c61
Mvq <mvpberger@gmail.com>
2024-11-03 14:47:37 +0100

    h

--- robo_rally.py
+++ robo_rally.py
@@ -310,7 +310,7 @@ try:
         if accepltable:
             print("Starting path planning")
             path_res = 15
-            expand_dis = 550
+            expand_dis = 1000
             rob = robot_models.PointMassModel(ctrl_range=[-path_res, path_res])
             
             _, local_coords = cam.next_map(True) 
