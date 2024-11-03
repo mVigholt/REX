c0331fe7c12c6a8100cbf338124dc8be0c4088ba
mVigholt <zwc822@alumni.ku.dk>
2024-11-03 11:50:45 +0100

    1

--- robo_rally.py
+++ robo_rally.py
@@ -62,7 +62,7 @@ landmark_colors = [CRED, CGREEN, CBLUE, CYELLOW] # Colors used when drawing the 
 # Allocate space for world map
 world = np.zeros((1000,1000,3), dtype=np.uint8)
 
-sequence = [6,2,3,4,6]
+sequence = [3]
 si = 0
 # Initialize particles
 num_particles = 100
