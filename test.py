e49879a70b42349ff3b82cca1a9493226c07d070
Mvq <mvpberger@gmail.com>
2024-11-03 17:12:13 +0100

    h

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import numpy as np
 import time
 import help as h
-import math
 
 otto = h.Arlo()
 
@@ -10,7 +9,11 @@ cam = h.Cam()
 speed = 60
 error = 2
 rotateSpeed = 31
-i = 4.0
 
 while True:
-  otto.Turn(math.pi)
+  input()
+  i = 4.33
+  otto.arlo.go_diff(rotateSpeed-1, rotateSpeed, 0, 1)
+  print("i = ", i)
+  time.sleep(i)
+  otto.arlo.stop()
